749718d859b7b05495d70ea79182880ac900be1b
roob-cell <dbatos01@gmail.com>
2022-09-13 18:27:27 +0200

    finish

--- 02-Js and CSS Clock/main.js
+++ 02-Js and CSS Clock/main.js
@@ -1,10 +1,21 @@
 const secondHand = document.querySelector(".second-hand")
+const minHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
 
 function  setDate(){
     const now = new Date()
+
     const seconds = now.getSeconds()
     const secondsDegree = ((seconds / 60) * 360) + 90
     secondHand.style.transform = `rotate(${secondsDegree}deg)`
+
+    const mins = now.getMinutes()
+    const minsDegree = ((mins / 60) * 360) + 90
+    minHand.style.transform = `rotate(${minsDegree}deg)`
+
+    const hour = now.getMinutes()
+    const hourDegree = ((hour / 12) * 360) + 90
+    hourHand.style.transform = `rotate(${hourDegree}deg)`
     
     
 }
