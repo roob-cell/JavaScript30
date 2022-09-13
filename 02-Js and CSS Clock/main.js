de949f0aafde80f138d96f7a6503591ee1d380c4
roob-cell <dbatos01@gmail.com>
2022-09-13 18:10:54 +0200

    seconds hand

--- 02-Js and CSS Clock/main.js
+++ 02-Js and CSS Clock/main.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo")
+const secondHand = document.querySelector(".second-hand")
+
+function  setDate(){
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const secondsDegree = ((seconds / 60) * 360) + 90
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`
+    
+    
+}
+
+setInterval(setDate, 1000)
