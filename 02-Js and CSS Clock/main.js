ebc1d5a6d6f2f87e9a2f15c4feb66d9d1e33f5a5
roob-cell <dbatos01@gmail.com>
2022-11-13 20:33:44 +0100

    k

--- 02-Js and CSS Clock/main.js
+++ 02-Js and CSS Clock/main.js
@@ -1,6 +1,6 @@
 const secondHand = document.querySelector(".second-hand")
 const minHand = document.querySelector(".min-hand")
-const hourHand = document.querySelector(".hour-hand")
+cons hourHand = document.querySelector(".hour-hand")
 
 function  setDate(){
     const now = new Date()
