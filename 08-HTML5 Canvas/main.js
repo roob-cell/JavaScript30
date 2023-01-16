59897b6ce7d2d82dce09dfdc731ffa52e60761f2
roob-cell <dbatos01@gmail.com>
2023-01-16 12:23:16 +0100

    k

--- 08-HTML5 Canvas/main.js
+++ 08-HTML5 Canvas/main.js
@@ -1,1 +1,33 @@
-console.log("Hola Mundo")
+const canvas = document.querySelector("#draw")
+const ctx = canvas.getContext("2d")
+
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+ctx.strokeStyle = "BADASS"
+ctx.lineJoin = "round"
+ctx.lineCap = "round"    
+
+let isDrawing = false
+let lastX = 0
+let lastY = 0
+
+canvas.addEventListener("mousemove", draw)
+canvas.addEventListener("mousedown", () => isDrawing = true)
+canvas.addEventListener("mouseup", () => isDrawing = false)
+canvas.addEventListener("mouseout", () => isDrawing = false)
+
+
+function draw(e){
+    if(!isDrawing) return
+    console.log(e)
+    ctx.beginPath()
+    //start from
+    ctx.moveTo(lastX, lastY)
+    //go to
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.stroke()
+    lastX = e.offsetX
+    lastY = e.offsetY
+
+}
