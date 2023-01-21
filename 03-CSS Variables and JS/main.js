b749d72393568533f6d10f8f59484f7c88f21e10
roob-cell <dbatos01@gmail.com>
2023-01-21 19:54:04 +0100

    h

--- 03-CSS Variables and JS/main.js
+++ 03-CSS Variables and JS/main.js
@@ -1,9 +1,9 @@
-cons inputs = document.querySelectorAll(".controls input")
+const inputs = document.querySelectorAll(".controls input")
 
 inputs.forEach( input => input.addEventListener("change", handleUpdate))
 inputs.forEach(input => input.addEventListener("mousemove", handleUpdate))
 
 function handleUpdate(){
     const suffix = this.dataset.sizing || ""
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+    ocument.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
